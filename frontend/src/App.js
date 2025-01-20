5091f8fb3f8fe0d908538a916c8bdced3aab49c6
vishnuvardhan938121 <147582100+vishnuvardhan938121@users.noreply.github.com>
2025-01-20 18:09:10 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/generate/test-cases', {
+            const response = await fetch('https://backend-test-case-gen-proj-3.onrender.com'/, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
